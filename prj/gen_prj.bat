42e32ec9b85bb0407f803be137d6f29b18c88806
Cristian Vasile <vasile.cristian@gmail.com>
2016-01-23 16:54:39 +0200

    - removed unneeded project.

--- prj/gen_prj.bat
+++ prj/gen_prj.bat
@@ -3,7 +3,7 @@ SET MAKETOOL=..\..\premake\release\premake5.exe
 
 %MAKETOOL% vs2013
 %MAKETOOL% --arch=ios xcode4
-%MAKETOOL% android_s2g
+
 %MAKETOOL% --arch=wp80app vs2012
 %MAKETOOL% --arch=w81app vs2013
 %MAKETOOL% --arch=wp81app vs2013
